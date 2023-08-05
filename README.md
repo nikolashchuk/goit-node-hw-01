391ea9f41fef6adf0f76c9159f019eb7d024a1fa
nikolashchuk <nikolashchuk@gmail.com>
2023-08-05 15:59:43 +0300

    add hw 1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-node-hw-01
+
+https://monosnap.com/file/GURIyqimZY3MtFqimmiood5AhusCKX
+https://monosnap.com/file/B4ivfnl5YtqJIAf6bwFwtWC3kytXqk
+https://monosnap.com/file/5V1Vxvxd6wqMOKqEVvWeLJGgv8WQvV
+https://monosnap.com/file/HbswZ1wiONX8Qb1vFdnzuIEljzfiYg
---
+++ index.js
@@ -0,0 +1,27 @@
+const argv = require("yargs").argv;
+const contacts = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
